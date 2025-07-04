c8f76cce496ec835796e7093173aacd499c31a98
Wolfgang Werner <mail@wolfgang-werner.net>
2025-07-04 17:14:51 +0200

    Enable deep linking

--- playground/component.js
+++ playground/component.js
@@ -2,6 +2,7 @@ window.onload = function() {
   const ui = SwaggerUIBundle({
     url: "./petstore-with-samples.json",
     dom_id: '#swagger-ui',
+    deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
